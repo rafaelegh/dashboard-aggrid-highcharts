75b8112fb8c2843af3507cd3ead68d6788c0caff
Rafael Gomez <rafael.gomez@leniolabs.com>
2023-04-04 17:01:45 -0300

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar/Sidebar'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <div className="AppGlass">
-        
+        <Sidebar />
       </div>
     </div>
   )
