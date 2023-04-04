cfe8888d370edee89e9c5f45a46db0b96e7982a6
Rafael Gomez <rafael.gomez@leniolabs.com>
2023-04-04 15:02:43 -0300

    container and css base

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Sidebar from './components/Sidebar'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-
+      <div className="AppGlass">
+        
+      </div>
     </div>
   )
 }
