697e11bdc02d80c29ec91d56031c22b74a8f6a18
Rafael Gomez <rafael.gomez@leniolabs.com>
2023-04-04 21:24:55 -0300

    add main dash

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Sidebar from './components/Sidebar/Sidebar'
+import MainDash from './components/MainDash/MainDash'
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <div className="AppGlass">
         <Sidebar />
+        <MainDash />
       </div>
     </div>
   )
---
+++ src/components/MainDash/MainDash.tsx
@@ -0,0 +1,10 @@
+import Table from '../Table/Table'
+
+export default function MainDash() {
+  return (
+    <div className="MainDash">
+        <h1>Dashboard</h1>
+        <Table />
+    </div>
+  )
+}
