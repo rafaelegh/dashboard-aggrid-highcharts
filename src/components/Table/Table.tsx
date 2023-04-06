76efc881346f5849df576636feb8cb0bd540f4a0
Rafael Gomez <rafael.gomez@leniolabs.com>
2023-04-06 20:50:56 -0300

    add changes on the table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -4,6 +4,11 @@ import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import { TableData, ITableData } from '../../Data/Data';
 import './Table.css';
+import StatusCellRenderer from './StatusCellRenderer';
+import DetailsCellRenderer from './DetailsCellRenderer';
+import { ColDef } from 'ag-grid-community';
+
+const rowWidth = 150;
 
 export default function Table() {
 
@@ -11,19 +16,28 @@ export default function Table() {
         { field: 'product' },
         { field: 'trackingId' },
         { field: 'date' },
-        { field: 'status' },
+        { field: 'status', cellRenderer: StatusCellRenderer },
+        { field: '', cellRenderer: DetailsCellRenderer }
     ]);
 
     const [rowData, setRowData] = useState(TableData);
 
+    const defaultColDef: ColDef = {
+        sortable: true,
+        filter: true,
+        resizable: true,
+        width: rowWidth
+    }
+
     return (
     <div className="table">
         <h3>Recent Orders</h3>
-            <div className="ag-theme-alpine" style={{ width: "100%", height: "50%" }}>
-                <AgGridReact
+            <div className="ag-theme-alpine" style={{ width: "100%", height: "70%" }}>
+                <AgGridReact<ITableData>
                     columnDefs={columnDefs}
                     rowData={rowData}
                     animateRows={true} rowSelection='multiple'
+                    defaultColDef={defaultColDef}
                 />
             </div>
     </div>
