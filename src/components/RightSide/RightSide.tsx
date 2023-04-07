804a6faf98a7da1866a4d27ba624a0878d704ef5
Rafael Gomez <rafael.gomez@leniolabs.com>
2023-04-06 21:16:13 -0300

    add right side correctly

---
+++ src/components/RightSide/RightSide.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function RightSide() {
+  return (
+    <div className="RightSide">
+        <div>
+            <h3>Updates</h3>
+        </div>
+        <div>
+            <h3>Customer Review</h3>
+        </div>
+    </div>
+  )
+}
